047889542b34020315f17602dc975c084eda8e24
Adron Hall <adronhall@gmail.com>
2013-07-29 00:07:19 -0700

    Added the following as a sample to run against test deployments with Windows Azure.

--- server.js
+++ server.js
@@ -1,13 +1,66 @@
-var restify = require('restify');
+var express = require('express');
+var app = express();
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-}
+var quotes = [
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+];
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+// body parser for parsing the post to add quotes.
+app.use(express.bodyParser());
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+// NOTE: Each of these URI points 
+
+// This gets ALL of the quotes back.
+app.get('/', function(req, res) {
+  res.json(quotes);
 });
+
+// This gets a random quote back.
+app.get('/quote/random', function(req, res) {
+  var id = Math.floor(Math.random() * quotes.length);
+  var q = quotes[id];
+  res.json(q);
+});
+
+// This gets a quote by id (ordered based on zero index in the array above)
+app.get('/quote/:id', function(req, res) {
+  if(quotes.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
+
+  var q = quotes[req.params.id];
+  res.json(q);
+});
+
+// 
+app.post('/quote', function(req, res) {
+  if(!req.body.hasOwnProperty('author') || 
+     !req.body.hasOwnProperty('text')) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+
+  var newQuote = {
+    author : req.body.author,
+    text : req.body.text
+  };
+
+  quotes.push(newQuote);
+  res.json(true);
+});
+
+app.delete('/quote/:id', function(req, res) {
+  if(quotes.length <= req.params.id) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
+
+  quotes.splice(req.params.id, 1);
+  res.json(true);
+});
+
+app.listen(process.env.PORT || 3412);
