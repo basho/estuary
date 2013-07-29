d86f9417ee10c90c28fa93d581cdd6e3d543a755
Adron Hall <adronhall@gmail.com>
2013-07-29 01:14:01 -0700

    Added a github.js file for the github library for collection of organization and related information from Basho.

---
+++ rivers/github.js
@@ -0,0 +1,18 @@
+var github = require('octonode');
+var client = github.client('a4c13f0b9abc0c89a5547caa255f015e46fef40e');
+var ghme   = client.me();
+var ghorg = client.org('basho');
+
+// client.get('/user', function (err, status, body) {
+//   console.log(body); //json object
+// });
+
+ghme.info(function(err, status, body){
+	console.log(status);
+});
+
+ghorg.info(function(err, data){
+	console.log(data);
+});
+
+console.log(ghorg);
